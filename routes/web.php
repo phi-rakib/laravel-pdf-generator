cab1d7ea77c8574b43568955df962560e07f3af8
rakib <phi.rakib@gmail.com>
2023-03-05 13:00:18 +0600

    added routes for employee index & pdf export

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(EmployeeController::class)->group(function() {
+    Route::get('employees', 'index');
+    Route::get('employees/pdf', 'exportToPDF')->name('employee.export.pdf');
 });
